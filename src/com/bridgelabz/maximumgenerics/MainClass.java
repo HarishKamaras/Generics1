0f3c9a1c3c5aa221010951cd45701624c1b1ac07
HarishKamaras <harishkamarasu@gmail.com>
2022-05-14 18:54:09 +0530

    FindMaximumInteger

--- src/com/bridgelabz/maximumgenerics/MainClass.java
+++ src/com/bridgelabz/maximumgenerics/MainClass.java
@@ -3,5 +3,9 @@ package com.bridgelabz.maximumgenerics;
 public class MainClass {
     public static void main(String[] args) {
         System.out.println("Welcome1 to generics");
+        //UC1 find maximum of three Integers
+        GenericClass genericClass = new GenericClass();
+        Integer printValue = genericClass.testMaxium(1,7,4);
+        System.out.println("The Maximum integer is : "+printValue);
     }
 }
